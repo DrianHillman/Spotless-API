a1ed032ef8b447c0bd06552777a6127ed4bf968e
Drian Hillman <dhillman24@gmail.com>
2017-04-12 21:34:38 -0400

    Working implementation of allPlaylists

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,14 +3,14 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  field :allPlaylists, types.String do
+  field :allPlaylists, types[Types::PlaylistType] do
     description "Playlists that belong to the user"
     resolve ->(obj, args, ctx) {
       Playlist.all.map { |x| x }
     }
   end
 
-  field :allIDs, types.String do
+  field :allIDs, types[String] do
     description "The id of all playlists"
     resolve ->(obj, args, ctx) {
       Playlist.all.map { |x| x.id }
